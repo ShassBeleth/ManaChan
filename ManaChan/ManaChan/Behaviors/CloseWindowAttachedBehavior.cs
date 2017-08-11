567435f6d84b56088153e5f32c7d28de37a3c9ee
ShassBeleth <shassbeleth@gmail.com>
2017-08-12 05:51:53 +0900

    画面を閉じる用のBehaviorを作成してViewModelから画面を閉じれるようにした

---
+++ ManaChan/ManaChan/Behaviors/CloseWindowAttachedBehavior.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows;
+
+namespace ManaChan.Behaviors {
+
+	/// <summary>
+	/// ウィンドウを閉じる添付ビヘイビア
+	/// </summary>
+	public class CloseWindowAttachedBehavior {
+
+		/// <summary>
+		/// プロパティの取得
+		/// </summary>
+		/// <param name="dependencyObject"></param>
+		/// <returns></returns>
+		public static bool GetClose( DependencyObject dependencyObject ) 
+			=> (bool)dependencyObject.GetValue( CloseProperty );
+
+		/// <summary>
+		/// プロパティの設定
+		/// </summary>
+		/// <param name="dependencyObject"></param>
+		/// <param name="value"></param>
+		public static void SetClose( DependencyObject dependencyObject , bool value ) 
+			=> dependencyObject.SetValue( CloseProperty , value );
+		
+		/// <summary>
+		/// IsCloseプロパティ作成
+		/// </summary>
+		public static readonly DependencyProperty CloseProperty = DependencyProperty.RegisterAttached( 
+			"Close" , 
+			typeof( bool ) , 
+			typeof( CloseWindowAttachedBehavior ) , 
+			new PropertyMetadata( false , OnCloseChanged ) 
+		);
+
+		/// <summary>
+		/// IsClose変化時イベント
+		/// Windowを閉じる
+		/// </summary>
+		/// <remarks>
+		/// この添付ビヘイビアがWindow以外のコントロールにつけられていた場合、
+		/// そのコントロールが属しているWindowを閉じる
+		/// </remarks>
+		/// <param name="dependencyObject"></param>
+		/// <param name="dependencyPropertyChangedEventArgs"></param>
+		private static void OnCloseChanged( 
+			DependencyObject dependencyObject , 
+			DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs 
+		) {
+
+			Window window = dependencyObject as Window ?? Window.GetWindow( dependencyObject );
+
+			if( GetClose( dependencyObject ) )
+				window.Close();
+
+		}
+
+	}
+
+}
--- ManaChan/ManaChan/ViewModels/Windows/MainWindowViewModel.cs
+++ ManaChan/ManaChan/ViewModels/Windows/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Prism.Mvvm;
+﻿using System;
+using Prism.Commands;
+using Prism.Mvvm;
 
 namespace ManaChan.ViewModels.Windows {
 
@@ -7,7 +9,22 @@ namespace ManaChan.ViewModels.Windows {
 	/// </summary>
 	public class MainWindowViewModel : BindableBase {
 
+		/// <summary>
+		/// 画面を閉じるかどうか
+		/// </summary>
+		private bool isCloseWindow = false;
+
+		/// <summary>
+		/// 画面を閉じるかどうか
+		/// </summary>
+		public bool IsCloseWindow {
+			set => SetProperty( ref this.isCloseWindow , value );
+			get => this.isCloseWindow;
+		}
+
 		#region 右クリックメニュー
+
+		#region 終了
 
 		/// <summary>
 		/// 終了文字列
@@ -22,7 +39,25 @@ namespace ManaChan.ViewModels.Windows {
 			get => this.quitHeaderOfContextMenu;
 		}
 
+		/// <summary>
+		/// 終了コマンド
+		/// </summary>
+		public DelegateCommand QuitCommandOfContextMenu;
+		
+		/// <summary>
+		/// 終了イベント
+		/// </summary>
+		/// <returns></returns>
+		private Action QuitExecuteOfContextMenu() => () => this.IsCloseWindow = true;
+
 		#endregion
+
+		#endregion
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		public MainWindowViewModel() => this.QuitCommandOfContextMenu = new DelegateCommand( this.QuitExecuteOfContextMenu() );
 
 	}
 

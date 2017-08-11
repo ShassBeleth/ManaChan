b369d101f9a8a81c35fbc8b2fbf7a5714eac6f2d
ShassBeleth <shassbeleth@gmail.com>
2017-08-12 02:27:45 +0900

    フレームワークのバージョン上げとファイル位置変更

---
+++ ManaChan/ManaChan/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace ManaChan.Views.Windows {
+
+	/// <summary>
+	/// MainWindow.xaml の相互作用ロジック
+	/// </summary>
+	public partial class MainWindow : Window {
+
+		public MainWindow() => InitializeComponent();
+
+	}
+
+}

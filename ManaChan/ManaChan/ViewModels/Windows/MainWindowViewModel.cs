43832a33ca1237a36d55318212f089620c7a5df2
ShassBeleth <shassbeleth@gmail.com>
2017-08-12 23:25:55 +0900

    画面いっぱいに描画 右クリックメニュー押下可否指定

--- ManaChan/ManaChan/ViewModels/Windows/MainWindowViewModel.cs
+++ ManaChan/ManaChan/ViewModels/Windows/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows;
 using Prism.Commands;
 using Prism.Mvvm;
 
@@ -8,6 +9,32 @@ namespace ManaChan.ViewModels.Windows {
 	/// MainWindowのViewModel
 	/// </summary>
 	public class MainWindowViewModel : BindableBase {
+
+		/// <summary>
+		/// 画面幅
+		/// </summary>
+		private double primaryScreenWidth = SystemParameters.PrimaryScreenWidth;
+
+		/// <summary>
+		/// 画面幅
+		/// </summary>
+		public double PrimaryScreenWidth {
+			private set => SetProperty( ref this.primaryScreenWidth , value );
+			get => this.primaryScreenWidth;
+		}
+
+		/// <summary>
+		/// 画面高さ
+		/// </summary>
+		private double primaryScreenHeight = SystemParameters.PrimaryScreenHeight;
+
+		/// <summary>
+		/// 画面高さ
+		/// </summary>
+		public double PrimaryScreenHeight {
+			private set => SetProperty( ref this.primaryScreenHeight , value );
+			get => this.primaryScreenHeight;
+		}
 
 		/// <summary>
 		/// 画面を閉じるかどうか
@@ -25,7 +52,7 @@ namespace ManaChan.ViewModels.Windows {
 		#region 右クリックメニュー
 
 		#region 終了
-
+		
 		/// <summary>
 		/// 終了文字列
 		/// </summary>
@@ -35,20 +62,47 @@ namespace ManaChan.ViewModels.Windows {
 		/// 終了文字列
 		/// </summary>
 		public string QuitHeaderOfContextMenu {
-			set => SetProperty( ref this.quitHeaderOfContextMenu , value );
+			private set => SetProperty( ref this.quitHeaderOfContextMenu , value );
 			get => this.quitHeaderOfContextMenu;
+		}
+
+		/// <summary>
+		/// 終了ボタン押下可否
+		/// </summary>
+		private bool isQuitEnabledOfContextMenu = true;
+
+		/// <summary>
+		/// 終了ボタン押下可否
+		/// </summary>
+		public bool IsQuitEnabledOfContextMenu {
+			private set => SetProperty( ref this.isQuitEnabledOfContextMenu , value );
+			get => this.isQuitEnabledOfContextMenu;
 		}
 
 		/// <summary>
 		/// 終了コマンド
 		/// </summary>
-		public DelegateCommand QuitCommandOfContextMenu;
-		
+		private DelegateCommand quitCommandOfContextMenu;
+
+		/// <summary>
+		/// 終了コマンド
+		/// </summary>
+		public DelegateCommand QuitCommandOfContextMenu {
+			private set => SetProperty( ref this.quitCommandOfContextMenu , value );
+			get => this.quitCommandOfContextMenu;
+		}
+
 		/// <summary>
 		/// 終了イベント
 		/// </summary>
 		/// <returns></returns>
 		private Action QuitExecuteOfContextMenu() => () => this.IsCloseWindow = true;
+
+		/// <summary>
+		/// 終了可否
+		/// </summary>
+		/// <returns></returns>
+		private Func<bool> QuitCanExecuteOfContextMenu() => () => this.IsQuitEnabledOfContextMenu;
 
 		#endregion
 
@@ -57,7 +111,7 @@ namespace ManaChan.ViewModels.Windows {
 		/// <summary>
 		/// コンストラクタ
 		/// </summary>
-		public MainWindowViewModel() => this.QuitCommandOfContextMenu = new DelegateCommand( this.QuitExecuteOfContextMenu() );
+		public MainWindowViewModel() => this.QuitCommandOfContextMenu = new DelegateCommand( this.QuitExecuteOfContextMenu() , this.QuitCanExecuteOfContextMenu() );
 
 	}
 

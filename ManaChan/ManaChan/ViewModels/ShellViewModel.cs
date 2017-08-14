b835c80bc7dc49319f593a92c2149a1eeda110e4
ShassBeleth <shassbeleth@gmail.com>
2017-08-14 17:37:25 +0900

    画面いっぱいに描画復活

---
+++ ManaChan/ManaChan/ViewModels/ShellViewModel.cs
@@ -0,0 +1,20 @@
+﻿using ManaChan.Models.ScreenSize;
+using Microsoft.Practices.Unity;
+using Prism.Mvvm;
+using System;
+
+namespace ManaChan.ViewModels {
+
+	/// <summary>
+	/// ShellのViewModel
+	/// </summary>
+	public class ShellViewModel : BindableBase {
+
+		[Dependency]
+		public PrimaryScreenSize PrimaryScreenSize { get; } = new PrimaryScreenSize();
+
+		public ShellViewModel() => Console.WriteLine( this.PrimaryScreenSize.ToString() );
+
+	}
+
+}

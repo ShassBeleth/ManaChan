fc8dbe7f51da06a141f7d3e9d3055df8a8b68c5d
ShassBeleth <shassbeleth@gmail.com>
2017-08-14 17:57:56 +0900

    キャラクターのデフォ位置を右下に設定

--- ManaChan/ManaChan/ViewModels/ShellViewModel.cs
+++ ManaChan/ManaChan/ViewModels/ShellViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using ManaChan.Models.ScreenSize;
 using Microsoft.Practices.Unity;
 using Prism.Mvvm;
-using System;
 
 namespace ManaChan.ViewModels {
 
@@ -10,10 +9,60 @@ namespace ManaChan.ViewModels {
 	/// </summary>
 	public class ShellViewModel : BindableBase {
 
+		/// <summary>
+		/// 画面サイズ
+		/// </summary>
 		[Dependency]
 		public PrimaryScreenSize PrimaryScreenSize { get; } = new PrimaryScreenSize();
 
-		public ShellViewModel() => Console.WriteLine( this.PrimaryScreenSize.ToString() );
+		#region キャラクターのサイズ
+
+		/// <summary>
+		/// キャラクターの最小の描画高さ
+		/// </summary>
+		private const int CharacterMinHeight = 300;
+
+		/// <summary>
+		/// キャラクターの描画高さ
+		/// </summary>
+		private int characterHeight = CharacterMinHeight;
+
+		/// <summary>
+		/// キャラクターの描画高さ
+		/// </summary>
+		public int CharacterHeight {
+			set => SetProperty( ref this.characterHeight , value );
+			get => this.characterHeight;
+		}
+
+		/// <summary>
+		/// キャラクターの最小の描画幅
+		/// </summary>
+		private const int CharacterMinWidth = 300;
+
+		/// <summary>
+		/// キャラクターの描画幅
+		/// </summary>
+		private int characterWidth = CharacterMinWidth;
+
+		/// <summary>
+		/// キャラクターの描画幅
+		/// </summary>
+		public int CharacterWidth {
+			set => SetProperty( ref this.characterWidth , value );
+			get => this.characterWidth;
+		}
+
+		#endregion
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		public ShellViewModel() {
+			
+
+			
+		}
 
 	}
 
